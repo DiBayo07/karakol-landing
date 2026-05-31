ab3a25075935ecc5ba2060ea39cdcb166d0eadd3
DiBayo07 <adahanbaydolotov@mail.ru>
2026-05-31 18:12:31 +0600

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import { AppDataProvider } from './context/AppDataContext'
 import Layout from './components/Layout'
 import AdminLayout from './pages/admin/AdminLayout'
